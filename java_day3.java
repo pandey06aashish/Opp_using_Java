d640b7b418493fd16263f44c6e5c7871f8fe6c3b
Aashish Pandey <124480555+pandey06aashish@users.noreply.github.com>
2023-09-24 12:30:39 +0530

    Add files via upload

---
+++ java_day3.java
@@ -0,0 +1,32 @@
+// Conditional Statement......
+// Q1. Check Eliglible to vote or not...
+
+// import java.util.*;
+// public class java_day3 {
+//     public static void main(String[] argu){
+//   Scanner sc=new Scanner(System.in);
+//   System.out.print("Enter Your Age :");
+//   int age=sc.nextInt();
+   
+//   if(age>=18){
+//     System.out.println("You are Eliglible to vote");
+//   }
+//  else{ System.out.println("You are not Eliglible to vote");
+// }
+//     }
+// }
+
+// Q2.Check Marks if less then 35 fail  else pass .....
+
+// import java.util.*;
+// public class java_day3{
+// public static void main(String[] argu){
+//     Scanner sc=new Scanner(System.in);
+//     System.out.print("Enter the Marks:");
+//     int marks=sc.nextInt();
+//    if(marks>=35){
+//     System.out.println("You are Pass");
+//    }
+//    else{System.out.println("You Are Fail");}
+// }
+// }
